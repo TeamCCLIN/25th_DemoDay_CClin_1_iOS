ca194855bcd4b6be00eadcbac936a3b160fa1fb4
Hong-Sen <20181053@sungshin.ac.kr>
2022-05-12 00:23:00 +0900

    [feat] mock up tab view

---
+++ CClin_iOS/Extension/Color+hex.swift
@@ -0,0 +1,23 @@
+//
+//  Color+hex.swift
+//  CClin_iOS
+//
+//  Created by 홍세은 on 2022/05/12.
+//
+
+import SwiftUI
+
+extension Color {
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        _ = scanner.scanString("#")
+        
+        var rgb: UInt64 = 0
+        scanner.scanHexInt64(&rgb)
+        
+        let r = Double((rgb >> 16) & 0xFF) / 255.0
+        let g = Double((rgb >>  8) & 0xFF) / 255.0
+        let b = Double((rgb >>  0) & 0xFF) / 255.0
+        self.init(red: r, green: g, blue: b)
+    }
+}
