b97da393a5aac1fa91d07821ee1f04a2aa578e94
Hong-Sen <20181053@sungshin.ac.kr>
2022-05-12 00:09:07 +0900

    Start project

---
+++ CClin_iOS/CClin_iOSApp.swift
@@ -0,0 +1,20 @@
+//
+//  CClin_iOSApp.swift
+//  CClin_iOS
+//
+//  Created by 홍세은 on 2022/05/12.
+//
+
+import SwiftUI
+
+@main
+struct CClin_iOSApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'CClin_iOS' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for CClin_iOS
+
+  # Realm DB
+  pod 'RealmSwift', '~>10'
+  
+  # Alomofire
+  pod 'Alamofire', '~> 5.5'
+
+end
