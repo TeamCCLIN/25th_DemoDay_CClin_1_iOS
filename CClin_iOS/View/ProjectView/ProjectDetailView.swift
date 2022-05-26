a91d875f34c3ee85a15d4f68677f491217122095
abi-hong <68041042+abi-hong@users.noreply.github.com>
2022-05-26 22:37:29 +0900

    [style] verify text align

--- CClin_iOS/View/ProjectView/ProjectDetailView.swift
+++ CClin_iOS/View/ProjectView/ProjectDetailView.swift
@@ -21,7 +21,7 @@ struct ProjectDetailView: View {
                     
                     Spacer()
                     
-                    VStack {
+                    VStack(alignment: .trailing) {
                         Text("국내 대학교 재/휴학생 모두")
                             .foregroundColor(Color.black)
                             .font(SpoqaHanSansNeo.bold(size: 13))
@@ -51,7 +51,7 @@ struct ProjectDetailView: View {
                     
                     Spacer()
                     
-                    VStack {
+                    VStack(alignment: .trailing) {
                         Text("신청")
                             .font(SpoqaHanSansNeo.bold(size: 13))
                             .foregroundColor(Color(hex: "474646"))
@@ -89,7 +89,7 @@ struct ProjectDetailView: View {
             VStack {
                 HStack {
                     Spacer()
-                    VStack {
+                    VStack(alignment: .trailing) {
                         Text("진행기간")
                             .font(SpoqaHanSansNeo.bold(size: 13))
                             .foregroundColor(Color(hex: "474646"))
@@ -131,7 +131,7 @@ struct ProjectDetailView: View {
                     
                     Spacer()
                     
-                    VStack {
+                    VStack(alignment: .trailing) {
                         Text("STEP 1_헤이홈 페르소나 설계")
                             .foregroundColor(Color.black)
                             .font(SpoqaHanSansNeo.bold(size: 13))
@@ -143,12 +143,19 @@ struct ProjectDetailView: View {
                             .foregroundColor(Color.black)
                             .font(SpoqaHanSansNeo.medium(size: 13))
                         
+                        
+                        Text("2022.06.22 - 2022.07.15")
+                            .foregroundColor(Color.white)
+                            .font(SpoqaHanSansNeo.bold(size: 10))
                         Spacer()
                         
                         Text("STEP 2_헤이홈 IoT 제품 기획전")
                             .foregroundColor(Color.black)
                             .font(SpoqaHanSansNeo.bold(size: 13))
-                        Text("- 해당 페르소나는 어떤 일상을 살아가고 있는 사람이고 그 과정에서 느낄 수 있는 불편함은 무엇일지 상상해보세요.")
+                        Text("- 페르소나가 겪을 불편함의 포인트를 발굴하고 이에 대한 ‘솔루션이 될 수 있는 기획전’을 제안해주세요.")
+                            .foregroundColor(Color.black)
+                            .font(SpoqaHanSansNeo.medium(size: 13))
+                        Text("- 단, 기획전의 구성은 헤이홈의 IoT 제품과 더불어 ‘함께 제안했을 때 묶음 구매 전환을 유도하는 데 부가적인 효과’를 줄 수 있는 타 업체의 제품 및 서비스를 결합해주세요.")
                             .foregroundColor(Color.black)
                             .font(SpoqaHanSansNeo.medium(size: 13))
                     }
@@ -171,7 +178,7 @@ struct ProjectDetailView: View {
                     
                     Spacer()
                     
-                    VStack {
+                    VStack(alignment: .trailing) {
                         Text("IoT 서비스 헤이홈 게시용 공고.pdf")
                             .foregroundColor(Color(hex: "F27953"))
                             .font(SpoqaHanSansNeo.bold(size: 13))
