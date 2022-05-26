f9e2b853221f8c3f606a6c8d9c5ffac587a1bf37
abi-hong <68041042+abi-hong@users.noreply.github.com>
2022-05-26 21:29:15 +0900

    [feat] complete project detail text:

--- CClin_iOS/View/ProjectView/ProjectDetailView.swift
+++ CClin_iOS/View/ProjectView/ProjectDetailView.swift
@@ -39,6 +39,7 @@ struct ProjectDetailView: View {
                 Rectangle()
                     .frame(height:1)
                     .foregroundColor(Color.gray_button_line)
+                    .padding(.horizontal, 40)
             }
             
             VStack {
@@ -77,17 +78,29 @@ struct ProjectDetailView: View {
                         Text("2022.06.22")
                             .foregroundColor(Color.gray)
                             .font(SpoqaHanSansNeo.bold(size: 10))
-                        
-                        Spacer()
-                        
-                        
-                        /*Text("진행기간")
+                    }
+                    
+                }
+                .padding(.horizontal, 40)
+            
+            }
+            
+            
+            VStack {
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("진행기간")
                             .font(SpoqaHanSansNeo.bold(size: 13))
                             .foregroundColor(Color(hex: "474646"))
                         Text("2022.06.22 - 2022.07.15")
                             .foregroundColor(Color.gray)
                             .font(SpoqaHanSansNeo.bold(size: 10))
                         
+                        
+                        Text("2022.06.22 - 2022.07.15")
+                            .foregroundColor(Color.white)
+                            .font(SpoqaHanSansNeo.bold(size: 10))
                         Spacer()
                         
                         
@@ -96,9 +109,49 @@ struct ProjectDetailView: View {
                             .foregroundColor(Color(hex: "474646"))
                         Text("2022.07.25")
                             .foregroundColor(Color.gray)
-                            .font(SpoqaHanSansNeo.bold(size: 10))*/
+                            .font(SpoqaHanSansNeo.bold(size: 10))
                     }
                     
+                }
+                .padding(.horizontal, 40)
+                .padding(.vertical, 10)
+                
+                Rectangle()
+                    .frame(height:1)
+                    .foregroundColor(Color.gray_button_line)
+                    .padding(.horizontal, 40)
+            }
+            
+            VStack {
+                
+                HStack {
+                    Text("프로젝트 설명")
+                        .font(SpoqaHanSansNeo.bold(size: 13))
+                        .foregroundColor(Color(hex: "474646"))
+                    
+                    Spacer()
+                    
+                    VStack {
+                        Text("STEP 1_헤이홈 페르소나 설계")
+                            .foregroundColor(Color.black)
+                            .font(SpoqaHanSansNeo.bold(size: 13))
+                        
+                        Text("- 헤이홈의 페르소나는 어떤 프로필을 보유하고 있는 사람일지 팀원들과 상의하여 하나의 캐릭터로 다듬어 보세요.")
+                            .foregroundColor(Color.black)
+                            .font(SpoqaHanSansNeo.medium(size: 13))
+                        Text("- 해당 페르소나는 어떤 일상을 살아가고 있는 사람이고 그 과정에서 느낄 수 있는 불편함은 무엇일지 상상해보세요.")
+                            .foregroundColor(Color.black)
+                            .font(SpoqaHanSansNeo.medium(size: 13))
+                        
+                        Spacer()
+                        
+                        Text("STEP 2_헤이홈 IoT 제품 기획전")
+                            .foregroundColor(Color.black)
+                            .font(SpoqaHanSansNeo.bold(size: 13))
+                        Text("- 해당 페르소나는 어떤 일상을 살아가고 있는 사람이고 그 과정에서 느낄 수 있는 불편함은 무엇일지 상상해보세요.")
+                            .foregroundColor(Color.black)
+                            .font(SpoqaHanSansNeo.medium(size: 13))
+                    }
                 }
                 .padding(.horizontal, 40)
                 .padding(.vertical, 10)
@@ -120,13 +173,39 @@ struct ProjectDetailView: View {
                     
                     VStack {
                         Text("IoT 서비스 헤이홈 게시용 공고.pdf")
-                            .foregroundColor(Color.orange)
+                            .foregroundColor(Color(hex: "F27953"))
                             .font(SpoqaHanSansNeo.bold(size: 13))
+                            .underline()
                         
                         Spacer()
                         
                         Text("프로젝트 제출 양식.ppt")
-                            .foregroundColor(Color.orange)
+                            .foregroundColor(Color(hex: "F27953"))
+                            .font(SpoqaHanSansNeo.bold(size: 13))
+                            .underline()
+                    }
+                }
+                .padding(.horizontal, 40)
+                .padding(.vertical, 10)
+                
+                Rectangle()
+                    .frame(height:1)
+                    .foregroundColor(Color.gray_button_line)
+                    .padding(.horizontal, 40)
+            }
+            
+            VStack {
+                
+                HStack {
+                    Text("기타 사항")
+                        .font(SpoqaHanSansNeo.bold(size: 13))
+                        .foregroundColor(Color(hex: "474646"))
+                    
+                    Spacer()
+                    
+                    VStack {
+                        Text("없음")
+                            .foregroundColor(Color.black)
                             .font(SpoqaHanSansNeo.bold(size: 13))
                     }
                 }
@@ -136,6 +215,22 @@ struct ProjectDetailView: View {
                 Rectangle()
                     .frame(height:1)
                     .foregroundColor(Color.gray_button_line)
+                    .padding(.horizontal, 40)
+            }
+            
+            Spacer()
+            Spacer()
+            
+            VStack {
+                Button(action: {}) {
+                    Text("프로젝트 신청하기")
+                        .foregroundColor(Color.white)
+                        .font(SpoqaHanSansNeo.bold(size: 13))
+                }
+                .tint(Color.main_club)
+                .buttonStyle(.borderedProminent)
+                .buttonBorderShape(.automatic)
+                .controlSize(.large)
             }
         }
         
